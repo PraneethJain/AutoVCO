fbea6fd3fad26c3a34b5748491940f38ec87f601
PraneethJain <praneethjain005@gmail.com>
2023-10-31 12:01:52 +0530

    read from serial monitor

--- src/predict.py
+++ src/predict.py
@@ -1,10 +1,16 @@
 from tensorflow import keras
 import numpy as np
 import pickle
+import serial
+
 
 with open("model.pk", "rb") as f:
     model = pickle.load(f)
-X = [[float(input("Temperature: ")), float(input("Voltage: "))]]
-predictions = model.predict(X)
-for i in range(len(predictions)):
-    print(f"Input: {X[i]}, Predicted Output: {predictions[i][0]}")
+ser = serial.Serial("COM11", 9600)
+while True:
+    b = ser.readline()
+    X = [list(map(float, b.split()))]
+    # X = [[float(input("Temperature: ")), float(input("Voltage: "))]]
+    predictions = model.predict(X)
+    for i in range(len(predictions)):
+        print(f"Input: {X[i]}, Predicted Output: {predictions[i][0]}")
