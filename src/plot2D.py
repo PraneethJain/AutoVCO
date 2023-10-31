953d82160d627c917f2a0716da5c4b9f97bf4e2c
PraneethJain <praneethjain005@gmail.com>
2023-10-31 15:22:55 +0530

    temperature frequency plot

---
+++ src/plot2D.py
@@ -0,0 +1,20 @@
+import csv
+import matplotlib.pyplot as plt
+
+with open(f"data/temperature_dataset.csv", newline="") as csvfile:
+        row1, row2 = csv.reader(csvfile, delimiter=",")
+        row1 = row1[1:]
+        row2 = row2[1:]
+
+X = []
+Y = []
+for parameters, res in zip(row1, row2):
+    temperature, _ = [s.split("=")[-1] for s in parameters.split()]
+    if res:
+        X.append(float(temperature))
+        Y.append(float(res))
+
+plt.plot(X, Y)
+plt.xlabel("Temperature (Celsius)")
+plt.ylabel("Frequency (Hz)")
+plt.show()
--- src/plot3D.py
+++ src/plot3D.py
@@ -1,5 +1,4 @@
 import matplotlib.pyplot as plt
-from matplotlib.ticker import MaxNLocator
 from matplotlib import cm
 
 X = []
