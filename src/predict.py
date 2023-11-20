02e70796a6d47c2d5d8e4c13710df1ef5a7e9007
PraneethJain <praneethjain005@gmail.com>
2023-11-20 16:27:32 +0530

    cleanup

--- src/predict.py
+++ src/predict.py
@@ -1,49 +1,33 @@
 from tensorflow import keras
 import numpy as np
 import pickle
-import serial
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import db
 from rich import print
 
 with open("model.pk", "rb") as f:
     model = pickle.load(f)
 
 
-# def listener(event):
-# data = curRef.get();
-# print(data)
-# actual_frequency = data["frequency"]
-actual_frequency = 647
-# X = [[data["temperature"], float(input("Control Voltage: ")), data["voltage"]]]
-X = [[29.0, 1.97, 3.75]]
+actual_frequency = float(input("Actual Frequency: "))
+X = [
+    [
+        float(input("Temperature: ")),
+        float(input("Control Voltage: ")),
+        float(input("Input Voltage: ")),
+    ]
+]
 predictions = model.predict(X)
 for i in range(len(predictions)):
     print(f"Input: {X[i]}, Predicted Output: {predictions[i][0]}")
-    predicted_frequency = predictions[i][0]           
+    predicted_frequency = predictions[i][0]
     percentage_error = (predicted_frequency - actual_frequency) / actual_frequency * 100
     print(f"{abs(percentage_error):.2f}% frequency error")
-    if (abs(percentage_error) < 5):
+    if abs(percentage_error) < 5:
         continue
 
-
     current_resistance = float(input("Enter resistance: "))
-    new_resistance = current_resistance*(100 - percentage_error)/100
+    new_resistance = current_resistance * (100 - percentage_error) / 100
     if percentage_error > 0:
         print(f"Decrease resistance by {abs(percentage_error):.2f}%")
     else:
         print(f"Increase resistance by {abs(percentage_error):.2f}%")
     print(f"New resistance: {new_resistance}")
-        
-
-# SERVICE_ACCOUNT_JSON_PATH = "/home/praneeth/Downloads/firebasekey.json"
-# cred = credentials.Certificate(SERVICE_ACCOUNT_JSON_PATH)
-# firebase_admin.initialize_app(
-#     cred,
-#     {
-#         "databaseURL": "https://vco-ml-default-rtdb.asia-southeast1.firebasedatabase.app"
-#     },
-# )
-# curRef = db.reference("cur")
-# curRef.listen(listener)
